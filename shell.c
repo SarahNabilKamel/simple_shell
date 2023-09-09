5fd13a6900678a1eff775e3897a005db6d71bf93
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-09-10 01:04:04 +0300

    hot fix in task 1

--- shell.c
+++ shell.c
@@ -2,40 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 1024
+#define COMMAND_MAX_LINE 1024
 
-int main()
+
+int main() 
 {
-    char command[MAX_COMMAND_LENGTH];
+    char* command_line = NULL;
+    size_t length = 0;
+    ssize_t read;
     pid_t pid;
-    while (true)
+    while(1) 
     {
+
         printf("#cisfun$ ");
         fflush(stdout);
 
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        read = getline(&command_line, &length, stdin);
+
+        if(read == -1)
         {
             break;
         }
 
-        command[strcspn(command, "\n")] = '\0';
+        command_line[strcspn(line, "\n")] = 0; 
 
         pid = fork();
-        if (pid == -1)
+
+        if(pid == -1) 
         {
-            perror("./shell: Error in fork");
+            perror("fork");
             exit(1);
-        }
-        else if (pid == 0)
-        {
-            if (execlp(command, command, NULL) == -1)
-            {
-                perror("./shell: No such file or directory");
-                exit(1);
-            }
+        } 
+        else if(pid == 0) 
+        { 
+
+            char* args[] = {strtok(line, " "), NULL};
+
+            execve(args[0], args, NULL);
+            perror("execve");
+            exit(1);
+
         }
         else
         {
