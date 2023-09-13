0f3c72115ab348944c2c3a7cbd22d39f245d9f4c
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-09-14 01:25:19 +0300

    update

---
+++ my_getline.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+
+
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
+{
+    char *newptr;
+    int c;
+    size_t i;
+    i = 0;
+    
+    if (lineptr == NULL || n == NULL || stream == NULL) 
+    {
+        return -1;
+    }
+
+   
+    if (*lineptr == NULL || *n == 0) 
+    {
+        *n = 128;  
+        *lineptr = (char *)malloc(*n);
+        if (*lineptr == NULL) {
+            return -1;  
+        }
+    }
+
+    
+    
+    
+    while ((c = fgetc(stream)) != EOF) 
+    {
+        
+        if (i >= *n - 1)
+        {
+            *n *= 2;
+            
+            newptr = (char *)realloc(*lineptr, *n);
+            if (newptr == NULL) 
+            {
+                return -1;  
+            }
+            *lineptr = newptr;
+        }
+
+        (*lineptr)[i++] = c;
+
+        if (c == '\n') {
+            break;  
+        }
+    }
+
+    (*lineptr)[i] = '\0';  
+
+    if (i == 0 && c == EOF) 
+    {
+        return -1;  
+    }
+
+    return i;  
+}
---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define COMMAND_MAX_LENGTH 1024
+
+void prompt();
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
+void my_exec( char *command );
+
+#endif
