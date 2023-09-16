df4549fa4b6b9dfac33938bf42a7a8064cc3d26c
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-09-16 01:06:36 -0700

    betty checks

--- my_env.c
+++ my_env.c
@@ -1,6 +1,11 @@
 #include "shell.h"
-
+/**
+ * is_env_command - checks if command line is env
+ * @command_line: pointer to command line
+ * Return: Always 0, positive value or negative value
+ */
 int is_env_command(char *command_line)
 {
-    return strcmp(command_line, "env") == 0;
+	return (strcmp(command_line, "env") == 0);
 }
+
