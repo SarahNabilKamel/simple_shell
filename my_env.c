cd142da614074835f8370c7541a5a4a69c9106e7
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-09-16 01:10:36 -0700

    Finalizing betty checks

--- my_env.c
+++ my_env.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
- * is_env_command - checks if command line is env
+ * is_env_command - checks if command line is env command
  * @command_line: pointer to command line
- * Return: Always 0, positive value or negative value
+ * Return: boolean
  */
 int is_env_command(char *command_line)
 {
--- my_exit.c
+++ my_exit.c
@@ -1,6 +1,11 @@
 #include "shell.h"
-
+/**
+ * is_exit_command - checks if command line is exit command
+ * @command_line: pointer to command line
+ * Return: boolean
+ */
 int is_exit_command(char *command_line)
 {
-    return strcmp(command_line, "exit") == 0;
+	return (strcmp(command_line, "exit") == 0);
 }
+
