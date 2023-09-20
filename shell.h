f7aaaaab6340e88390de33e3311df0f151d67f08
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-09-20 10:14:54 -0700

    remove old unnesecary prototypes and minor cleaning

--- shell.h
+++ shell.h
@@ -12,22 +12,20 @@ extern char **environ;
 
 #define COMMAND_MAX_LENGTH 1024
 
-void prompt(void);
 size_t my_strcspn(const char *str, const char *reject);
 size_t my_strlen(const char *str);
 char *my_strcpy(char *dest, const char *src);
 char *my_strcat(char *dest, const char *src);
 char *my_strdup(const char *str);
 int my_strcmp(const char *str1, const char *str2);
-void my_exec(char *command);
-void execwitharg(char *command);
 void prompt2(void);
 void exec3(char **args, int *status, char *executable);
 void print_environment(char **envp);
 int is_env_command(char *command_line);
 int is_exit_command(char *command_line);
 void execute_command(char *command, char **args, int *status);
-void find_executable(char *command, char **args, char *path, int *status, char *prog);
+void find_executable(char *command, char **args,
+char *path, int *status, char *prog);
 int is_whitespace(char c);
 
 char **f_token(char *command);
@@ -35,3 +33,4 @@ void array_free(char **array);
 char *get_value(char *key);
 
 #endif
+
