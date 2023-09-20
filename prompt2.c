ea96b54f73148534a3e000653e65ff32e30e2970
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-09-20 10:13:39 -0700

    Fixing formatting and minor structure changes

--- prompt2.c
+++ prompt2.c
@@ -1,13 +1,15 @@
 #include "shell.h"
-
 /**
- * prompt2 - start of line
- * Return: void
- */
+* prompt2 - start of line
+* Return: void
+*/
 void prompt2(void)
 {
 	char prompt[] = "$ ";
+
+
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 	fflush(stdout);
 }
+
