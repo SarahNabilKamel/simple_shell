f8507a43efcc88256677ea220ff6aebd5e09f166
AbdelRahman Hanafy <abdelrahmansam@gmail.com>
2023-09-20 10:17:59 -0700

    more Betty comments and minor fixes

--- stringman2.c
+++ stringman2.c
@@ -1,34 +1,53 @@
 #include "shell.h"
-
+/**
+ * my_strlen - length of string
+ * @str: Pointer to string
+ *
+ * Return: returns length of string
+ */
 size_t my_strlen(const char *str)
 {
-    const char *s = str;
-    while (*s)
-    {
-        s++;
-    }
-    return s - str;
-}
+	const char *s = str;
 
+	while (*s)
+	{
+		s++;
+	}
+	return (s - str);
+}
+/**
+ * my_strdup - duplicates a string
+ * @str: Pointer to source string
+ *
+ * Return: pointer copy
+ */
 char *my_strdup(const char *str)
 {
-    size_t len = my_strlen(str) + 1;
-    char *copy = malloc(len);
+	size_t len = my_strlen(str) + 1;
+	char *copy = malloc(len);
 
-    if (copy != NULL)
-    {
-        char *p = copy;
-        while (*str)
-        {
-            *p++ = *str++;
-        }
-        *p = '\0';
-    }
+	if (copy != NULL)
+	{
+		char *p = copy;
 
-    return copy;
+		while (*str)
+		{
+			*p++ = *str++;
+		}
+		*p = '\0';
+	}
+
+	return (copy);
 }
-
+/**
+ * is_whitespace - checks for white space
+ * @c: character to check
+ *
+ * Return: true or false
+ */
 int is_whitespace(char c)
 {
-    return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v';
+	return (c == ' ' || c == '\t' || c == '\n' ||
+	c == '\r' || c == '\f' || c == '\v');
 }
+
